08dd09846ca4427fa355f472cfb9e1ed991bc928
trmg <tg.rdmg@gmail.com>
2021-10-23 01:05:26 -0300

    Pessoa inserir esqueleto imcompleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Router, RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { MenuComponent } from "./menu/menu.component";
+import { PessoaAlterarComponent } from "./pessoa/pessoa-alterar/pessoa-alterar.component";
 
 const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'entrar', component: MenuComponent}
+    {path: 'entrar', component: MenuComponent},
+    {path: 'alterarPerfil', component: PessoaAlterarComponent}
 ]
 
 @NgModule({
--- src/app/pessoa/pessoa-alterar/pessoa-alterar.component.ts
+++ src/app/pessoa/pessoa-alterar/pessoa-alterar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-pessoa-alterar',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaAlterarComponent implements OnInit {
 
+  // public form: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    // this.form = new FormGroup({
+    //   peso: new FormControl(null, {
+    //     validators: [Validators.required, Validators.minLength(3)]
+    //   }),
+    //   email: new FormControl (null, {
+    //     validators: [Validators.required, Validators.email]
+    //   }),
+    //   telefone: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(8)]
+    //   }),
+    //   celular: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(9)]
+    //   }),
+    //   logradouro:new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(9)]
+    //   }),
+    //   numero: new FormControl (null, {
+    //     validators: [Validators.required, Validators.email]
+    //   }),
+    //   complemento: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(0)]
+    //   }),
+    //   cep: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(8)]
+    //   }),
+    //   bairro: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(5)]
+    //   }),
+    //   cidade: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(5)]
+    //   }),
+    //   estado: new FormControl (null, {
+    //     validators: [Validators.required, Validators.minLength(2)]
+    //   })
+    // })
   }
 
 }
